0639d1327623f7238ed6d4f1bb077d0f4638b4ca
Chris Kubick <socalledsound@gmail.com>
2021-01-07 11:27:08 -0800

    update

--- test-sketch/index.js
+++ test-sketch/index.js
@@ -1,1 +1,26 @@
-alert('hello everyone')
+const regl = require('regl')();
+const glslify  = require('glslify');
+
+const bgCol = [Math.random(), Math.random(), Math.random(), Math.random()];
+// const bgCol = [1.0, 0.0,0.0,1.0];
+
+const drawBG = regl({
+    frag: glslify('./frag.glsl'),
+    vert: glslify('./vert.glsl'),
+
+attributes: {
+    position: [[-1,-1], [1, -1], [1, 1],[-1,1]]
+},
+
+uniforms: {
+
+    color: bgCol,
+},
+
+count: 4,
+primitive: "triangle fan",
+});
+
+regl.frame(() => {
+    drawBG();
+})
